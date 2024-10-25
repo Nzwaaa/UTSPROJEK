e1bfee2a07f71e01e11c66421cb1b4b054b7fc99
Nazwa Hemalia P <109130882+Nzwaaa@users.noreply.github.com>
2024-10-25 14:29:11 +0700

    comite1

---
+++ app/src/main/java/lat/pam/utsproject/MainActivity.kt
@@ -0,0 +1,55 @@
+package lat.pam.utsproject
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putBoolean("isLoggedIn", false)  // Hapus status login
+            apply()
+        }
+
+        val edtUsername: EditText = findViewById(R.id.etUsername)
+        val edtPassword: EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = edtUsername.text.toString()
+            val password = edtPassword.text.toString()
+
+            if (username == "admin" && password == "1234") {
+
+                with(sharedPref.edit()) {
+                    putBoolean("isLoggedIn", true)
+                    apply()
+                }
+
+                startActivity(Intent(this, ListFoodActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Login gagal. Coba lagi.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/lat/pam/utsproject/OrderActivity.kt
@@ -0,0 +1,59 @@
+package lat.pam.utsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+
+    private lateinit var servingsEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var notesEditText: EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+
+        val foodName = intent.getStringExtra("foodName")
+
+        val foodNameTextView: TextView = findViewById(R.id.etFoodName)
+        foodNameTextView.text = foodName
+
+
+        servingsEditText = findViewById(R.id.etServings)
+        nameEditText = findViewById(R.id.etName)
+        notesEditText = findViewById(R.id.etNotes)
+
+        val orderButton: Button = findViewById(R.id.btnOrder)
+        orderButton.setOnClickListener {
+
+            val servings = servingsEditText.text.toString()
+            val orderingName = nameEditText.text.toString()
+            val additionalNotes = notesEditText.text.toString()
+
+            // Start ConfirmationActivity with the user's input data
+            val intent = Intent(this, ConfirmationActivity::class.java).apply {
+                putExtra("foodName", foodName)
+                putExtra("servings", servings)
+                putExtra("orderingName", orderingName)
+                putExtra("additionalNotes", additionalNotes)
+            }
+            startActivity(intent)
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
